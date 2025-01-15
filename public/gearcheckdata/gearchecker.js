e484ed7d097cd5c8777965ccfe7a21a5735e36ed
ranidspace <ranidspace@proton.me>
2025-01-15 15:20:19 -0500

    Begin rewrite of gearcheck
    This will probably be redone but whatever idfk i'm just saving my
    progress in this branch cuz i'm redoing the main site.

--- public/gearcheckdata/gearchecker.js
+++ public/gearcheckdata/gearchecker.js
@@ -1,85 +1,96 @@
-const BASE_URL = "https://raw.githubusercontent.com/Leanny/splat3/main";
-function submit() {
+// Constants
+
+const database = "https://raw.githubusercontent.com/Leanny/splat3/main/";
+
+jfetch(database + "versions.json").then((data) => {
+  ver = data.at(-1);
+});
+
+// Functions
+
+async function submit() {
   //clear all data on the page
   document.getElementById("output").innerHTML = "";
 
-  //grab data
-  const gearfile = document.getElementById("geardata").files[0];
-  //read data
-  const reader = new FileReader();
-  reader.readAsText(gearfile);
-
-  //do things with the data
-  reader.onload = async function () {
-    const result = JSON.parse(reader.result);
-
-    //prepping html stuff
-    let div = document.createElement("details");
-    div.className = "toggle";
-    div.id = "missingear";
-    div.open = true;
-    document.getElementById("output").appendChild(div);
-
-    div = document.createElement("summary");
-    div.className = "lbl-toggle";
-    div.id = "missingtitle";
-    div.textContent = `Missing Gear`;
-    document.getElementById("missingear").appendChild(div);
-
-    div = document.createElement("div");
-    div.className = "gear-container";
-    div.id = "Missing";
-    document.getElementById("missingear").appendChild(div);
-
-    const ver = (await jfetch(BASE_URL + "/versions.json")).at(-1);
-
-    console.log(`The current splatoon version is ${ver.split("").join(".")}`);
-    //run the missing gear function
-    await missinggear(result, "head", "Head", ver);
-    await missinggear(result, "clothing", "Clothes", ver);
-    await missinggear(result, "shoes", "Shoes", ver);
-
-    document
-      .getElementById("output")
-      .appendChild(document.createElement("hr"));
-
-    //Loop through 0-5 stars and find the gear for each
-    for (let i = 0; i < 6; i++) {
+  // get data from the provided geardata file
+  new Response(document.getElementById("geardata").files[0]).json().then(
+    (userGear) => {
+      //prepping html stuff
       let div = document.createElement("details");
       div.className = "toggle";
-      div.id = `collapsible${i}`;
+      div.id = "missingear";
       div.open = true;
       document.getElementById("output").appendChild(div);
 
       div = document.createElement("summary");
       div.className = "lbl-toggle";
-      div.textContent = i + " Star Gear";
-      document.getElementById(`collapsible${i}`).appendChild(div);
+      div.id = "missingtitle";
+      div.textContent = `Missing Gear`;
+      document.getElementById("missingear").appendChild(div);
 
       div = document.createElement("div");
-      div.className = "gear-container collapsible-content";
-      div.id = String(i);
-      document.getElementById(`collapsible${i}`).appendChild(div);
+      div.className = "gear-container";
+      div.id = "Missing";
+      document.getElementById("missingear").appendChild(div);
 
-      await starcount(result, "headGear", "GearInfoHead", i, ver);
-      await starcount(result, "clothingGear", "GearInfoClothes", i, ver);
-      await starcount(result, "shoesGear", "GearInfoShoes", i, ver);
-      if (i != 5) {
-        document
-          .getElementById("output")
-          .appendChild(document.createElement("hr"));
+      console.log(
+        `The current splatoon version is ` +
+          ver.substr(0, 1) +
+          "." +
+          ver.substr(1, 1) +
+          "." +
+          ver.substr(2, 1),
+      );
+      // run the missing gear function
+      missinggear(userGear, "head", "Head");
+      missinggear(userGear, "clothing", "Clothes");
+      missinggear(userGear, "shoes", "Shoes");
+
+      document
+        .getElementById(`output`)
+        .appendChild(document.createElement("hr"));
+
+      // Loop through 0-5 stars and find the gear for each
+      for (let i = 0; i < 6; i++) {
+        let div = document.createElement("details");
+        div.className = "toggle";
+        div.id = `collapsible${i}`;
+        div.open = true;
+        document.getElementById(`output`).appendChild(div);
+
+        div = document.createElement("summary");
+        div.className = "lbl-toggle";
+        div.textContent = i + ` Star Gear`;
+        document.getElementById(`collapsible${i}`).appendChild(div);
+
+        div = document.createElement("div");
+        div.className = "gear-container collapsible-content";
+        div.id = String(i);
+        document.getElementById(`collapsible${i}`).appendChild(div);
+
+        starcount(userGear, "headGear", "GearInfoHead", i);
+        starcount(userGear, "clothingGear", "GearInfoClothes", i);
+        starcount(userGear, "shoesGear", "GearInfoShoes", i);
+        if (i != 5) {
+          document
+            .getElementById(`output`)
+            .appendChild(document.createElement("hr"));
+        }
       }
-    }
-  };
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
 }
 
-// This function shows the missing gear for
-async function missinggear(gearlist, gear, gearInfo, ver) {
-  const path = gearlist["gear"]["data"][gear + "Gears"]["nodes"];
+// this function shows the missing gear for
+async function missinggear(gearlist, Gear, GearInfo) {
+  const path = gearlist["gear"]["data"][Gear + "Gears"]["nodes"];
   const jsonlistunfiltered = await jfetch(
-    BASE_URL + `/data/mush/${ver}/GearInfo${gearInfo}.json`
+    database + `data/mush/${ver}/GearInfo${GearInfo}.json`,
   );
-  const translate = await jfetch(BASE_URL + "/data/language/USen.json");
+  const translate = await jfetch(database + `data/language/USen.json`);
 
   let jsonlist = [];
   // TODO: improve performace. apparently for loops are faster than filtering.
@@ -90,64 +101,63 @@ async function missinggear(gearlist, gear, gearInfo, ver) {
         x.HowToGet === "Impossible" ||
         x.Season > ver.substring(0, 1) ||
         x.__RowId === "Clt_HAP001"
-      )
+      ),
   );
-  const diff = jsonlist.filter(
-    (entry1) => !path.some((entry2) => entry1.Id === entry2[gear + "GearId"])
+  const difference = jsonlist.filter(
+    (entry1) => !path.some((entry2) => entry1.Id === entry2[Gear + "GearId"]),
   );
 
-  for (i = 0; i < diff.length; i++) {
+  for (i = 0; i < difference.length; i++) {
     let div = document.createElement("div");
     div.className = "item";
     if (
-      diff[i].__RowId.substr(4, 3) === "AMB" ||
-      (diff[i].__RowId.substr(4, 3) === "MSN" && diff[i].HowToGet === "Other")
+      difference[i].__RowId.substr(4, 3) === "AMB" ||
+      (difference[i].__RowId.substr(4, 3) === "MSN" &&
+        difference[i].HowToGet === "Other")
     ) {
       div.classList.add("amiiboitem");
     }
-    if (diff[i].HowToGet === "Uroko") {
+    if (difference[i].HowToGet === "Uroko") {
       div.classList.add("urokoitem");
     }
-    div.id = String(gear + String(i));
+    div.id = String(Gear + String(i));
     document.getElementById("Missing").appendChild(div);
 
     div = document.createElement("img");
-    div.src = BASE_URL + `/images/gear/${diff[i]["__RowId"]}.png`;
+    div.src = database + `/images/gear/${difference[i]["__RowId"]}.png`;
     div.alt =
-      translate["CommonMsg/Gear/GearName_" + gearInfo][
-        diff[i]["__RowId"].substr(4)
+      translate["CommonMsg/Gear/GearName_" + GearInfo][
+        difference[i]["__RowId"].substr(4)
       ];
-    document.getElementById(gear + String(i)).appendChild(div);
+    document.getElementById(Gear + String(i)).appendChild(div);
 
     div = document.createElement("span");
     div.className = "gearname";
     div.textContent =
-      translate["CommonMsg/Gear/GearName_" + gearInfo][
-        diff[i]["__RowId"].substr(4)
+      translate["CommonMsg/Gear/GearName_" + GearInfo][
+        difference[i]["__RowId"].substr(4)
       ];
-    document.getElementById(gear + i).appendChild(div);
+    document.getElementById(Gear + i).appendChild(div);
   }
 }
-//This function shows gear for a certain star count
-async function starcount(gearlist, gear, gearInfo, stars, ver) {
-  const path = gearlist.gear.data[gear + "s"].nodes;
-  const jsonList = await jfetch(
-    BASE_URL + `/data/mush/${ver}/${gearInfo}.json`
-  );
+// starcount:  create divs for every gear for each star level
+async function starcount(gearlist, Gear, GearInfo, Stars) {
+  const path = gearlist.gear.data[Gear + "s"].nodes;
+  const jsonlist = await jfetch(database + `data/mush/${ver}/${GearInfo}.json`);
 
   for (let i = 0; i < path.length; i++) {
-    if (path[i].rarity === stars) {
+    if (path[i].rarity === Stars) {
       let div = document.createElement("div");
 
       div.className = "item";
       div.id = path[i].name;
-      document.getElementById(stars).appendChild(div);
+      document.getElementById(Stars).appendChild(div);
 
       const gearitem =
-        jsonList[jsonList.findIndex((obj) => obj.Id === path[i][gear + "Id"])];
+        jsonlist[jsonlist.findIndex((obj) => obj.Id === path[i][Gear + "Id"])];
 
       div = document.createElement("img");
-      div.src = BASE_URL + `/images/gear/${gearitem.__RowId}.png`;
+      div.src = database + `images/gear/${gearitem.__RowId}.png`;
       div.alt = path[i].name;
       document.getElementById(path[i].name).appendChild(div);
 
