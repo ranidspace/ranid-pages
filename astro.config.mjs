94bf9d35b86b5bbe5d4e87d85b69b5def087f429
ranidspace <ranidspace@proton.me>
2025-03-13 15:51:46 -0400

    Fix an issue with mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://ranid.space",
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap()],
 });
